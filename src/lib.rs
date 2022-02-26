6d1747df0b19e6c383f94bffa83864f53c8366f7
Julius Michaelis <glitter@liftm.de>
2022-02-26 16:15:11 +0900

    more snip

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,23 @@
 // No idea where they took it from
 pub const KAFKA_SEED: u32 = 0x9747b28c;
 
+// Integers in rust aren't generic. :/
+macro_rules! round {
+    ($m:expr, $h:expr, $k:expr) => {
+        $k.wrapping_mul($m).slack(24).wrapping_mul($m) ^ $h.wrapping_mul($m)
+    };
+}
+
+macro_rules! short_round {
+    ($m:expr, $h:expr, $r:expr, $T:ty) => {{
+        let r = $r;
+        match r.is_empty() {
+            false => ($h ^ r.iter().rev().fold(0, |r, &i| (i as $T) | (r << 8))).wrapping_mul($m),
+            true => $h,
+        }
+    }};
+}
+
 pub fn murmur2(data: &[u8], seed: u32) -> u32 {
     let len = data.len();
     let m = 0x5bd1e995u32;
@@ -9,18 +26,9 @@ pub fn murmur2(data: &[u8], seed: u32) -> u32 {
 
     let mut chunks = data.chunks_exact(4);
     let h = (&mut chunks).fold(h, |h, k| {
-        u32::from_le_bytes(k.try_into().unwrap())
-            .wrapping_mul(m)
-            .slack(24)
-            .wrapping_mul(m)
-            ^ h.wrapping_mul(m)
+        round!(m, h, u32::from_le_bytes(k.try_into().unwrap()))
     });
-
-    let r = chunks.remainder();
-    let h = match r.is_empty() {
-        false => (h ^ r.iter().rev().fold(0, |r, &i| (i as u32) | (r << 8))).wrapping_mul(m),
-        true => h,
-    };
+    let h = short_round!(m, h, chunks.remainder(), u32);
 
     h.slack(13).wrapping_mul(m).slack(15)
 }
@@ -39,12 +47,7 @@ pub fn murmur64a(data: &[u8], seed: u64) -> u64 {
             .wrapping_mul(m))
         .wrapping_mul(m)
     });
-
-    let r = chunks.remainder();
-    let h = match r.is_empty() {
-        false => (h ^ r.iter().rev().fold(0, |r, &i| (i as u64) | (r << 8))).wrapping_mul(m),
-        true => h,
-    };
+    let h = short_round!(m, h, chunks.remainder(), u64);
 
     h.slack(47).wrapping_mul(m).slack(47)
 }
@@ -57,26 +60,13 @@ pub fn murmur64b(data: &[u8], seed: u64) -> u64 {
 
     let mut chunks = data.chunks_exact(4);
     let (h1, h2) = (&mut chunks).fold((h1, h2), |(h1, h2), k| {
-        (
-            h2,
-            u32::from_le_bytes(k.try_into().unwrap())
-                .wrapping_mul(m)
-                .slack(24)
-                .wrapping_mul(m)
-                ^ h1.wrapping_mul(m),
-        )
+        (h2, round!(m, h1, u32::from_le_bytes(k.try_into().unwrap())))
     });
-
     let (h1, h2) = match len % 8 > 3 {
         true => (h2, h1),
         false => (h1, h2),
     };
-
-    let r = chunks.remainder();
-    let h2 = match r.is_empty() {
-        false => (h2 ^ r.iter().rev().fold(0, |r, &i| (i as u32) | (r << 8))).wrapping_mul(m),
-        true => h2,
-    };
+    let h2 = short_round!(m, h2, chunks.remainder(), u32);
 
     let h1 = (h1 ^ h2 >> 18).wrapping_mul(m);
     let h2 = (h2 ^ h1 >> 22).wrapping_mul(m);
