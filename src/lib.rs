930ff5dd114ad22af84ffd232b928220e00b1203
Julius Michaelis <glitter@liftm.de>
2022-02-27 00:49:11 +0900

    crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! # murmur2 Hash Functions
+//!
+//! This is a pure Rust implementation of the murmur2 hash functions.
+//! It is tested against the [original C++ implementation](https://github.com/aappleby/smhasher/).
+//! To keep this crate `no_std` and dependency free,
+//! the tests live in a different crate, which is not published to crates.io.
+//! The implementations have not been particularly optimized for performance.
+//!
+//! The original C++ implementations are architecture/endianness dependent.
+//! The only independent function provided is `MurmurHashNeutral2`.
+//! This crate does not follow that scheme,
+//! all functions are provided in an endianness-dependent and -independent version.
+//! The endianness-dependent versions have their name suffixed with a `ne`, for "native endian".
+#![cfg_attr(not(test), no_std)]
+
 #[macro_use]
 mod hlp;
 mod imp;
@@ -47,6 +62,6 @@ pub fn murmur64bne(data: &[u8], seed: u64) -> u64 {
     imp::murmur64b(data, seed, u32::from_ne_bytes)
 }
 
-/// Seed found in Kafka source.
+/// Seed found in [Kafka source](https://github.com/apache/kafka/blob/3.1.0/clients/src/main/java/org/apache/kafka/common/utils/Utils.java#L479).
 // No idea where they took it from
 pub const KAFKA_SEED: u32 = 0x9747b28c;
